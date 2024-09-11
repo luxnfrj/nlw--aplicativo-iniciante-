d4914e2f60651a945d3ef693ff283e12fa62dc02
Luiza <luizaasth8@gmail.com>
2024-09-11 10:58:53 -0300

    Fundamentos 005

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-let meta = {
-    value: 'ler um livro por mês',
-    checked: true,
+function start() {
+    console.log('comecou')
 }
 
-let metas = [
-    meta,
-    {
-        value: "caminhar 20 minutos todos os dias",
-        checked: false
-    }
-]
-
-console.log(metas[1].value)
+start()
