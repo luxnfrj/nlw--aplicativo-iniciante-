032718d2d57b2cd6d08ec34cf7daa5e3c68e4344
Luiza <luizaasth8@gmail.com>
2024-09-11 11:35:01 -0300

    Fundamentos 007

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 function start() {
-    let count = 0
-    while(count <= 10){
-        console.log(count)
-        count = count + 1
+    while(true){
+        let opcao = "sair"
+        switch(opcao) {
+            case "cadastrar":
+                console.log("Vamos cadastrar")
+                break
+            case "listar":
+                console.log("Vamos listar")
+                break
+            case "sair":
+                return    
+        }
     }
 }
 
