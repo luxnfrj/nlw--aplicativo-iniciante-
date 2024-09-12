a8fc5ae53fa17ce88131d5dc9b73423480c99280
Luiza <luizaasth8@gmail.com>
2024-09-12 00:30:21 -0300

    deletar metas

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas realizadas " + realizadas.length,
+        message: "Metas realizadas: " + realizadas.length,
         choices: [...realizadas]
     })
 }
@@ -73,9 +73,34 @@ const metasAbertas = async () => {
     } 
 
     await select({
-        message: "Metas Abertas " + abertas.length,
+        message: "Metas Abertas: " + abertas.length,
         choices: [...abertas]
     })
+}
+
+const deletarMetas = async () => {
+    const metasDesmarcadas = metas.map((meta) => {
+        return { value: meta.value, checked: false }
+    })
+
+    const itemsADeletar = await checkbox({    
+        message: "Selecione um item para deletar",
+        choices: [...metasDesmarcadas],
+        instructions: false,
+    })
+    
+    if(itemsADeletar.length ==0) {
+        console.log("Nenhum item para deletar!")
+        return
+    }
+
+    itemsADeletar.forEach((item) => {
+        metas = metas.filter((meta) => {
+            return meta.value != item
+        })
+    })
+
+    console.log("Meta(s) deletada(s) com sucesso")
 }
 
 const start = async () => {
@@ -102,6 +127,10 @@ const start = async () => {
                     value: "abertas"
                 },
                 {
+                    name: "Deletar metas",
+                    value: "deletar"
+                },
+                {
                     name: "sair",
                     value: "sair"
                 }
@@ -122,6 +151,9 @@ const start = async () => {
             case "abertas":
                 await metasAbertas()
                 break
+            case "deletar":
+                await deletarMetas()
+                break    
             case "sair":
                 console.log("Até a próxima")
                 return    
--- todo.md
+++ todo.md
@@ -3,5 +3,5 @@
     - [x] Metas realizadas
     - [x] Metas abertas
 - [x] Marcar/Desmarcar metas como concluida
-- Remover metas
+- [x] Remover metas
 - Sistema de mensagens
