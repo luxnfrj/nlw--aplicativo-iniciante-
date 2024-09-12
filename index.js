b33beb7fa88b364cb81b0d0b6b462f6a632e6b6d
Luiza <luizaasth8@gmail.com>
2024-09-12 00:05:54 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -57,8 +57,24 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas realizadas",
+        message: "Metas realizadas " + realizadas.length,
         choices: [...realizadas]
+    })
+}
+// água[] - caminhar [] - cantar [x]
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
+    
+    if(abertas.length ==0) {
+        console.log("Não existem metas abertas! :")
+        return
+    } 
+
+    await select({
+        message: "Metas Abertas " + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -82,6 +98,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "sair",
                     value: "sair"
                 }
@@ -98,6 +118,9 @@ const start = async () => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log("Até a próxima")
--- todo.md
+++ todo.md
@@ -1,7 +1,7 @@
 - [x] Criar metas
 - [x] Listar metas
     - [x] Metas realizadas
-    - Metas abertas
+    - [x] Metas abertas
 - [x] Marcar/Desmarcar metas como concluida
 - Remover metas
 - Sistema de mensagens
