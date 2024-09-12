85bc00ba3a840a4971bbc7f33a09b579e7f92f2d
Luiza <luizaasth8@gmail.com>
2024-09-11 23:51:24 -0300

    resolve problema marcadas/desmarcadas

--- index.js
+++ index.js
@@ -27,15 +27,14 @@ const listarMetas = async () => {
         choices: [...metas],
         instructions: false,
     })
+    metas.forEach((m) => {
+        m.checked = false
+    })
+    
     if(respostas.length ==0) {
         console.log("Nenhuma meta foi selecionada!")
         return
     }
-
-    metas.forEach((m) => {
-        m.checked = false
-    })
-
     respostas.forEach((resposta) => {
         const meta = metas.find((m) => {
             return m.value == resposta
